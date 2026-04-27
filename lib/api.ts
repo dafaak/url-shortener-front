991f6d3323bfb03dc53a3f87da8bd11d5fb7ad94
dafaak <israel.pe@outlook.es>
2026-04-27 16:40:14 -0500

    refactor: api

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
@@ -6,7 +7,7 @@ const api = axios.create({
 
 // Interceptor para añadir el token de Go a todas las peticiones
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = Cookies.get('auth_token');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
